e5b18173edd8935d8667976b7db4a894af23ac6d
nurkholis <nur.kholis@ist.id>
2022-12-14 13:32:13 +0700

    add: tableview programatically

--- TestProgramaticallyUIKIT/Core/FirstScreen.swift
+++ TestProgramaticallyUIKIT/Core/FirstScreen.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
-class FirstScreen: UIViewController {
 
+struct Person {
+    let id: String
+    let name: String
+}
+
+class FirstScreen: UIViewController {
     lazy var nextButton: UIButton = {
         let btn = UIButton()
         btn.configuration = .filled()
@@ -37,11 +42,36 @@ class FirstScreen: UIViewController {
         return btn
     }()
     
+    lazy var tableView: UITableView = {
+        let tbl = UITableView()
+        return tbl
+    }()
+    
+    let data: [Person] = [
+            Person(id: "1", name: "Ahmad"),
+            Person(id: "2", name: "Bagas"),
+            Person(id: "3", name: "Rio"),
+            Person(id: "4", name: "Ivan"),
+            Person(id: "1", name: "David"),
+            Person(id: "1", name: "Martin")
+    ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "first screen"
-        setup()
+//        setup()
+        tableView = UITableView(frame: .zero)
+        self.view = tableView
+        setupTbl()
+    
+    }
+    
+    func setupTbl() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.showsVerticalScrollIndicator = false
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
     func setup() {
@@ -49,7 +79,9 @@ class FirstScreen: UIViewController {
         // add component to supeview
         view.addSubview(nextButton)
         view.addSubview(_testView)
+        
         _testView.addSubview(subButton)
+        _testView.addSubview(tableView)
         
         nextButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
         
@@ -67,17 +99,25 @@ class FirstScreen: UIViewController {
             _testView.topAnchor.constraint(equalTo: nextButton.bottomAnchor, constant: 8.0),
             _testView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             _testView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -20),
-//            _testView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+//            _testView.bottomAnchor.constraint(equalTo: tableView.bottomAnchor, constant: -20),
 //            _testView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
-            _testView.heightAnchor.constraint(equalToConstant: 100)
+            _testView.heightAnchor.constraint(equalToConstant: 1000)
         ])
         
         // To activate nscontraint subbutton
         NSLayoutConstraint.activate([
             subButton.centerXAnchor.constraint(equalTo: _testView.centerXAnchor, constant: 15),
-            subButton.centerYAnchor.constraint(equalTo: _testView.centerYAnchor),
+            subButton.topAnchor.constraint(equalTo: _testView.topAnchor, constant: 10),
+            subButton.bottomAnchor.constraint(equalTo: tableView.bottomAnchor, constant: -20),
             subButton.widthAnchor.constraint(equalToConstant: 200),
             subButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        // to activate nsconstraint tablecell
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: subButton.topAnchor, constant: 10),
+            tableView.bottomAnchor.constraint(equalTo: _testView.bottomAnchor, constant: -20),
+//            tableView.centerYAnchor.constraint(equalTo: _testView.centerYAnchor)
         ])
     
     }
@@ -86,6 +126,22 @@ class FirstScreen: UIViewController {
         let vc = SecondScreen()
         vc.title = "second screen"
         navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+
+extension FirstScreen: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomTableViewCell {
+            cell.textLabel?.text = data[indexPath.row].name
+            cell.setupCell(image: "PlaceholderImage")
+            return cell
+        }
+        fatalError("could not dequeueReusableCell")
     }
 }
 
