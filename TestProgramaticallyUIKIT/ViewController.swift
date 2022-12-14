093b5777c5e7079b68de8b0bd878595371bc140e
nurkholis <nur.kholis@ist.id>
2022-12-14 11:07:03 +0700

    Initial Commit

---
+++ TestProgramaticallyUIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProgramaticallyUIKIT
+//
+//  Created by nur kholis on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
